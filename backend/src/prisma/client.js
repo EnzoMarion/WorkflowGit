91266b51f1ada14483112be294066820c08beccb
EnzoMarion <marionenzo26@gmail.com>
2026-01-14 10:40:37 +0100

    fix(backend): make prisma work with postgres in Docker

--- backend/src/prisma/client.js
+++ backend/src/prisma/client.js
@@ -1,21 +1,7 @@
-let prisma;
+const { PrismaClient } = require('@prisma/client');
 
-try {
-  const { PrismaClient } = require('@prisma/client');
-
-  prisma = new PrismaClient({
-    log:
-        process.env.NODE_ENV === 'development'
-            ? ['query', 'info', 'warn', 'error']
-            : ['error'],
-  });
-} catch (e) {
-  console.warn('Prisma désactivé ou non disponible, client mocké.');
-
-  prisma = {
-    $connect: async () => {},
-    $disconnect: async () => {},
-  };
-}
+const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+});
 
 module.exports = prisma;
